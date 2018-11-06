a0cbdff66ba414ea025bd3e762e9749736fe4eb7
Maxim Markelow <maxim.markelow@siisltd.ru>
2018-11-06 18:23:38 +0300

    Fixed argument null exception in PostgreMigrationOptions

--- src/Marvin.Migrations.PostgreSQL/MigratorBuilderExtensions.cs
+++ src/Marvin.Migrations.PostgreSQL/MigratorBuilderExtensions.cs
@@ -10,11 +10,11 @@ namespace Marvin.Migrations.PostgreSQL
         /// </summary>
         /// <param name="builder"></param>
         /// <param name="connectionString">Connection string to Postgre</param>
-        /// <param name="migrationTableHistoryName">Migration history table name. If param <see langword="null"/> default value will be used</param>
-        /// <param name="databaseEncoding">Text presentation of database encoding for Postgre. If <see langword="null"/> default value will be used</param>
-        /// <param name="lcCollate"> String sort order for Postgre. If param <see langword="null"/> default value will be used</param>
-        /// <param name="lcCtype">Character classification for Postgre. If param <see langword="null"/> default value will be used</param>
-        /// <param name="connectionLimit">Limit of connections to Postgre. If param <see langword="null"/> default value will be used</param>
+        /// <param name="migrationTableHistoryName">Migration history table name. If param <see langword="null"/> default value will be used (<see cref="PostgreDbProviderOptions.DefaultMigrationTableName"/>)</param>
+        /// <param name="databaseEncoding">Text presentation of database encoding for Postgre. If <see langword="null"/> default value will be used (<see cref="PostgreDbProviderOptions.DefaultDatabaseEncoding"/>)</param>
+        /// <param name="lcCollate"> String sort order for Postgre. If param <see langword="null"/> default value will be used (<see cref="PostgreDbProviderOptions.DefaultLC_COLLATE"/>)</param>
+        /// <param name="lcCtype">Character classification for Postgre. If param <see langword="null"/> default value will be used (<see cref="PostgreDbProviderOptions.DefaultLC_CTYPE"/>)</param>
+        /// <param name="connectionLimit">Limit of connections to Postgre. If param <see langword="null"/> default value will be used (<see cref="PostgreDbProviderOptions.DefaultConnectionLimit"/>)</param>
         public static MigratorBuilder UsePostgreSQL(
             this MigratorBuilder builder,
             string connectionString,
@@ -26,11 +26,11 @@ namespace Marvin.Migrations.PostgreSQL
         {
             var options = new PostgreDbProviderOptions(
                 connectionString,
-                migrationTableHistoryName,
-                databaseEncoding,
-                lcCollate,
-                lcCtype,
-                connectionLimit);
+                migrationTableHistoryName ?? PostgreDbProviderOptions.DefaultMigrationTableName,
+                databaseEncoding ?? PostgreDbProviderOptions.DefaultDatabaseEncoding,
+                lcCollate ?? PostgreDbProviderOptions.DefaultLC_COLLATE,
+                lcCtype ?? PostgreDbProviderOptions.DefaultLC_CTYPE,
+                connectionLimit ?? PostgreDbProviderOptions.DefaultConnectionLimit);
             builder.UserDbProviderFactory(new PostgreDbProviderFactory(options));
             return builder;
         }
