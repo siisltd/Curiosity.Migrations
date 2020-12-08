de9ed12d1d939813b1dc87cf25869733ab5dd715
Markeli <markelow.dev+gitlab@gmail.com>
2020-12-08 12:01:53 +0300

    #18 Fixed transaction tests

--- src/Curiosity.Migrations/Migrations/ScriptMigrationBatch.cs
+++ src/Curiosity.Migrations/Migrations/ScriptMigrationBatch.cs
@@ -13,24 +13,35 @@ namespace Curiosity.Migrations
         /// <summary>
         /// Batch order in a migration
         /// </summary>
-        public int OrderIndex { get; set; }
+        public int OrderIndex { get; }
             
         /// <summary>
         /// Batch name (shown at logs, name is a text after --BATCH:)
         /// </summary>
-        public string? Name { get; set; }
+        public string? Name { get; }
         
         /// <summary>
         /// SQL
         /// </summary>
-        public string Script { get; set; }
+        public string Script {
+            get => _script;
+            internal set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                    throw new ArgumentNullException(nameof(Script));
+
+                _script = value;
+            }
+        }
+
+        private string _script;
 
         public ScriptMigrationBatch(
             int orderIndex,
             string? name,
             string script)
         {
-            if (orderIndex <= 0) throw new ArgumentOutOfRangeException(nameof(orderIndex));
+            if (orderIndex < 0) throw new ArgumentOutOfRangeException(nameof(orderIndex));
             if (String.IsNullOrWhiteSpace(script)) throw new ArgumentNullException(nameof(orderIndex));
             
             OrderIndex = orderIndex;
